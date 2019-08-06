d07b30a7922545ddb08b28952e96d8c2e286fabc
Himanshu507 <HimanshuRoy507@outlook.com>
2019-08-06 07:02:32 -0700

    Something Change

--- app/src/main/java/com/hoax/animatedrecyclerview/MainActivity.java
+++ app/src/main/java/com/hoax/animatedrecyclerview/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         mData.add(new SetData("Product 1","Take strikingly clear photos with the 12MP + 5MP AI dual camera. 1.25μm large pixels on the primary lens capture the beauty of any scene","$50",R.drawable.ic_launcher_background));
         mData.add(new SetData("Product 1","Take strikingly clear photos with the 12MP + 5MP AI dual camera. 1.25μm large pixels on the primary lens capture the beauty of any scene","$50",R.drawable.ic_launcher_background));
 
-
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         SimpleRecyclerAdapter adapter = new SimpleRecyclerAdapter(this,mData);
